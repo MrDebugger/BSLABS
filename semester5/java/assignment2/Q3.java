20db09b0d4caaa04df9b6fd058d2e7070ed9570e
Ijaz Ur Rahim ( The Alien ) <ijazkhan095@GMAIL.COM>
2020-10-27 22:09:26 -0700

    Create Q3.java

---
+++ semester5/java/assignment2/Q3.java
@@ -0,0 +1,23 @@
+class Q3{
+  public static void main(String[] args){
+    String myStr1 = "Hello";
+    String myStr1 = "World";
+    System.out.println(myStr1.charAt(0));
+    
+    System.out.println(myStr1.contains("Hel"));
+    System.out.println(myStr2.contains("Hel"));
+    
+    System.out.println(myStr1.endsWith("Hel"));
+    System.out.println(myStr1.endsWith("Hello"));
+    
+    System.out.println(myStr1.indexOf("l"));
+    System.out.println(myStr2.endsWith("rl"));
+    
+    System.out.println(myStr1.length());
+    
+    System.out.println(myStr1.toUpperCase());
+    
+    System.out.println(myStr1.toLowerCase());
+    
+  }
+}
