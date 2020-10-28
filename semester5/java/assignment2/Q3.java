8ee8bea1490358d31377ac361cc04c6dfa442c4b
Ijaz Ur Rahim ( The Alien ) <ijazkhan095@GMAIL.COM>
2020-10-27 22:20:38 -0700

    Update Q3.java

--- semester5/java/assignment2/Q3.java
+++ semester5/java/assignment2/Q3.java
@@ -2,22 +2,22 @@ class Q3{
   public static void main(String[] args){
     String myStr1 = "Hello";
     String myStr1 = "World";
-    System.out.println(myStr1.charAt(0));
+    System.out.println(myStr1.charAt(0)); //H
     
-    System.out.println(myStr1.contains("Hel"));
-    System.out.println(myStr2.contains("Hel"));
+    System.out.println(myStr1.contains("Hel")); //true
+    System.out.println(myStr2.contains("Hel")); //false
     
-    System.out.println(myStr1.endsWith("Hel"));
-    System.out.println(myStr1.endsWith("Hello"));
+    System.out.println(myStr1.endsWith("Hel")); //true
+    System.out.println(myStr1.endsWith("lo")); //false
     
-    System.out.println(myStr1.indexOf("l"));
-    System.out.println(myStr2.endsWith("rl"));
+    System.out.println(myStr1.indexOf("l")); //2
+    System.out.println(myStr2.endsWith("rl")); //2
     
-    System.out.println(myStr1.length());
+    System.out.println(myStr1.length()); // 5
     
-    System.out.println(myStr1.toUpperCase());
+    System.out.println(myStr1.toUpperCase()); //HELLO
     
-    System.out.println(myStr1.toLowerCase());
+    System.out.println(myStr2.toLowerCase()); //WORLD
     
   }
 }
